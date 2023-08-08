6498751dcb663663ea5b4c200dad2c5c96a901c7
David Eugène <edspeedy@gmail.com>
2023-08-08 19:38:27 +0200

    Modification du listing 5-1 et tests-ed.js

---
+++ tests-ed.js
@@ -0,0 +1,9 @@
+let lastName = "Eugène";
+let firstName = "David";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+firstName = "Gabriel";
+console.log(fullName);
+fullName = firstName + " " + lastName;
+console.log(fullName);
+
